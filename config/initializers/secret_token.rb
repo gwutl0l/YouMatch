d9cc4427e7a4f6cfecb09c94ca961fa6661c5e0c
Georges Duplessy <georges023@gmail.com>
2010-12-16 17:19:22 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Podil::Application.config.secret_token = '90f04d2ae5d2b81ab1452a4376841fe301453878d5a40b08d7cb946da0a950a73ae8502bef37b4a44a5cc43cd26780d63655b55cfce98f204cced81f55078b8b'
