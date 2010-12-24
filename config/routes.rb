0acd6569a22cf8f8b5596a737a5f5d1f4dffe3ac
Georges Henry <georges023@gmail.com>
2010-12-24 16:27:27 -0500

    DRY'd up code, created main structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Podil::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
-
-  get "pages/about"
-
-  get "pages/help"
+  root :to => 'pages#home'
+  match '/about', :to => 'pages#about'
+  match '/contact', :to => 'pages#contact'
+  match '/help', :to => 'pages#help'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
