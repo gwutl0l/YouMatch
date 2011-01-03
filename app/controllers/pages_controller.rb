c6e0e610d5ad2b33e39c4c39620e32a1e3abb772
Georges Henry <georges023@gmail.com>
2011-01-02 21:53:48 -0500

    removed similar artist

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,14 +21,14 @@ class PagesController < ApplicationController
   end
 
   def search
-    @title   = "Results"
-    search  = params['q']
-    @quer   = search
-    client   = YouTubeG::Client.new
-    @youtube = client.videos_by(:query => "#{search}", :page => 1, :per_page => 1)
-    @msearch = Search.music_hash
-    lfm = LastFM.new()
-    @similar = lfm.artist.getSimilar(:artist => params['q'], :limit => 5)
+    @title    = "Results"
+    search    = params['q']
+    @quer     = search
+    client    = YouTubeG::Client.new
+    @youtube  = client.videos_by(:query => "#{search}", :page => 1, :per_page => 1)
+    @msearch  = Search.music_hash
+    lfm       = LastFM.new()
+    @similar  = lfm.artist.getSimilar(:artist => search, :limit => 5)
     @simitr   = lfm.track.getSimilar(:track => 'tik tok', :artist => 'ke$ha', :limit => 5)
   end
 end
